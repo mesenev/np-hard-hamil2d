5d879dc9e6e0e6e53da4f4fc75a623fe19141ed1
Mesenev Pavel <heredemonsnearby@mail.ru>
2024-11-23 21:39:54 +1000

    0,0-9 matrix_h fix

--- easy.py
+++ easy.py
@@ -6,90 +6,57 @@ import numpy as np
 graph_g = set()
 POWER = 0
 
-
-def get_sector_vertices(sector_size: int, sector_idx: int) -> set[int]:
-    return set(range(sector_idx * sector_size, (sector_idx + 1) * sector_size))
-
-
-def subgraph_idx_to_original_vertices(sector_size: int, idx: int) -> tuple[int, set[int]]:
-    subgraphs_sector_size = 2 ** sector_size
-    sector_idx = idx // subgraphs_sector_size
-    in_sector_idx = idx - subgraphs_sector_size * sector_idx
-    result = set(index for index, bit in enumerate(bin(in_sector_idx)[2:]) if bit == '1')
-    # in_sector_idx - битовая маска, в которой
-    # бит i обозначает вхождение вершины (sector_idx * sector_size + i)
-    # в множество с номером idx
-    # TODO: заполнить result
-    return sector_idx, result
-
-
-def calc_subgraph_edge_weight(weights: list[list[float]], i: int, j: int) -> float:
-    sector_size = len(weights) // 3
-    i_sector, i_vertices = subgraph_idx_to_original_vertices(sector_size, i)
-    j_sector, j_vertices = subgraph_idx_to_original_vertices(sector_size, j)
-    if i_sector == j_sector:
-        return 0
-    result = 0
-    result += sum(weights[x][y] for x in i_vertices for y in get_sector_vertices(sector_size, i_sector) - i_vertices)
-    result += sum(weights[x][y] for x in i_vertices for y in get_sector_vertices(sector_size, j_sector) - j_vertices)
-    result += sum(weights[x][y] for x in j_vertices for y in get_sector_vertices(sector_size, i_sector) - i_vertices)
-    return result
-
-
-def count_weight_for_vertex_pair(vertex1, shift_1, vertex2, shift_2):
-    included = set(
-        index + shift_1 for index, bit in
-        enumerate(f'{vertex1:b}'.ljust(POWER, '0')) if bit == '1'
-    )
-    excluded = set(
-        index + shift_2 for index, bit in
-        enumerate(f'{vertex2:b}'.ljust(POWER, '0')) if bit == '0'
-    )
-
-    answer = sum(
-        1 for pair in product(
-            included,
-            excluded
-        ) if tuple(sorted(pair)) in graph_g)
-
-    return answer
+MASK = 0  #(1 << POWER) - 1  # Создаем маску из всех единиц длиной k бит
 
 
 def invert_bits(num):
-    mask = (1 << POWER) - 1  # Создаем маску из всех единиц длиной k бит
-    return ~num & mask  # Инвертируем биты числа и обрезаем до k бит
+    return ~num & MASK  # Инвертируем биты числа и обрезаем до k бит
+
+
+def count_weight_for_vertex_pair(vertex1, shift_1, vertex2, shift_2):
+    incl_bin_set = list(enumerate("".join(reversed(f'{vertex1:b}')).ljust(POWER, '0')))
+    excl_bin_set = list(enumerate("".join(reversed(f'{vertex2:b}')).ljust(POWER, "0")))
+    included = set(
+        index + shift_1 for index, bit in incl_bin_set if bit == '1'
+    )
+    excluded = set(
+        index + shift_2 for index, bit in excl_bin_set if bit == '0'
+    )
+    answer = sum(1 for pair in product(included, excluded) if pair in graph_g)
+
+    return answer
 
 
 def get_matrix_h(n):
     combinations_amount = 2 ** POWER
     matrix_h = np.zeros((3 * combinations_amount, 3 * combinations_amount), dtype=int)
-    # todo: починить этот обосравшийся цикл
+
     for sector, vertex in product(range(3), range(combinations_amount)):
-        shift_src = sector * POWER
+        vertex_shift = sector * POWER
         weight_inside_sector = count_weight_for_vertex_pair(
-            vertex, shift_src, vertex, shift_src
+            vertex, vertex_shift, vertex, vertex_shift
         )
 
-        shift_trg = ((sector + 1) % 3) * POWER
-        src_to_trg = 0
+        combo_shift = ((sector + 1) % 3) * POWER
         for combo in range(combinations_amount):
-            src_to_trg += count_weight_for_vertex_pair(
-                vertex, shift_src, combo, shift_trg
+            row, column = (vertex + sector * combinations_amount, combo + ((sector + 1) % 3) * combinations_amount)
+            src_to_trg = count_weight_for_vertex_pair(
+                vertex, vertex_shift, combo, combo_shift
             )
             src_to_trg += count_weight_for_vertex_pair(
-                combo, shift_trg, vertex, shift_src
+                combo, combo_shift, vertex, vertex_shift
             )
-            row, column = vertex + shift_src, combo + shift_trg
             weight = src_to_trg + weight_inside_sector
 
-            matrix_h[row, column] = weight if weight else -1
-            matrix_h[column, row] = weight if weight else -1
+
+            matrix_h[row, column] = weight
+            matrix_h[column, row] = weight
 
     return matrix_h
 
 
 def main():
-    global POWER
+    global POWER, MASK
     with open("input.txt") as f:
         n, *data = f.readlines()
 
@@ -97,9 +64,11 @@ def main():
     n += 3 - n % 3  # Добавляем вершины до кратности 3,
     # Чтобы равномерно разбить их на соответствующие участки
     POWER = n // 3
+    MASK = (1 << POWER) - 1  # Создаем маску из всех единиц длиной k бит
 
     for src, target in (tuple(map(lambda x: int(x) - 1, line.split())) for line in data):
-        graph_g.add((min(src, target), max(src, target)))
+        graph_g.add((src, target))
+        graph_g.add((target, src))
 
     matrix_h = get_matrix_h(n)
     pprint(matrix_h)
