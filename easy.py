01cad8abbb7213af3ddf036c043b97dd0dbc4dbe
Mesenev Pavel <heredemonsnearby@mail.ru>
2024-11-23 18:36:37 +1000

    matrix-issue

--- easy.py
+++ easy.py
@@ -1,6 +1,10 @@
-from itertools import permutations, combinations
+from itertools import product
+from pprint import pprint
 
 import numpy as np
+
+graph_g = set()
+POWER = 0
 
 
 def get_sector_vertices(sector_size: int, sector_idx: int) -> set[int]:
@@ -32,56 +36,75 @@ def calc_subgraph_edge_weight(weights: list[list[float]], i: int, j: int) -> flo
     return result
 
 
-# Это написано неправильно
-# def count_sub_weight_for_vertex(length, vertex, graph_g, n_subset=0):
-#     """
-#     Считаем вес в рамках одной кучи для вершины и её отрицания. w(x1, y1)
-#     :param graph_g:
-#     :param length:
-#     :param vertex:
-#     :return:
-#     """
-#     included = set(index for index, bit in enumerate(f'{vertex: b}'.ljust(length, '0')) if bit == '1')
-#
-#     weight = 0
-#
-#     for i in range(length):  #
-#         if i in included:
-#             continue
-#         weight += sum([(length * n_subset + i, length * n_subset + j) in graph_g for j in range(length)])
-#     return weight
+def count_weight_for_vertex_pair(vertex1, shift_1, vertex2, shift_2):
+    included = set(
+        index + shift_1 for index, bit in
+        enumerate(f'{vertex1:b}'.ljust(POWER, '0')) if bit == '1'
+    )
+    excluded = set(
+        index + shift_2 for index, bit in
+        enumerate(f'{vertex2:b}'.ljust(POWER, '0')) if bit == '0'
+    )
 
-def count_weight_for_vertex_pair(vertex1, n_sub1, vertex2, n_sub2):
-    included = set(index for index, bit in enumerate(f'{vertex1: b}'.ljust(vertex1, '0')) if bit == '1')
-    excluded = set(index for index, bit in enumerate(f'{vertex2: b}'.ljust(vertex2, '0')) if bit == '0')
-    pass
+    answer = sum(
+        1 for pair in product(
+            included,
+            excluded
+        ) if tuple(sorted(pair)) in graph_g)
 
-
-def get_matrix_h(n, matrix_g):
-    combinations_amount = 2 ** (len(matrix_g) // 3)
-    matrix_h = np.zeros((3 * combinations_amount, 3 * combinations_amount), dtype=int)
-
-    for vertex in combinations_amount:
-        weight = count_weight_for_vertex_pair((len(matrix_g) // 3), vertex, graph_g)
-
-    answer = list()
     return answer
 
 
+def invert_bits(num):
+    mask = (1 << POWER) - 1  # Создаем маску из всех единиц длиной k бит
+    return ~num & mask  # Инвертируем биты числа и обрезаем до k бит
+
+
+def get_matrix_h(n):
+    combinations_amount = 2 ** POWER
+    matrix_h = np.zeros((3 * combinations_amount, 3 * combinations_amount), dtype=int)
+    # todo: починить этот обосравшийся цикл
+    for sector, vertex in product(range(3), range(combinations_amount)):
+        shift_src = sector * POWER
+        weight_inside_sector = count_weight_for_vertex_pair(
+            vertex, shift_src, vertex, shift_src
+        )
+
+        shift_trg = ((sector + 1) % 3) * POWER
+        src_to_trg = 0
+        for combo in range(combinations_amount):
+            src_to_trg += count_weight_for_vertex_pair(
+                vertex, shift_src, combo, shift_trg
+            )
+            src_to_trg += count_weight_for_vertex_pair(
+                combo, shift_trg, vertex, shift_src
+            )
+            row, column = vertex + shift_src, combo + shift_trg
+            weight = src_to_trg + weight_inside_sector
+
+            matrix_h[row, column] = weight if weight else -1
+            matrix_h[column, row] = weight if weight else -1
+
+    return matrix_h
+
+
 def main():
+    global POWER
     with open("input.txt") as f:
         n, *data = f.readlines()
 
     n = int(n)
     n += 3 - n % 3  # Добавляем вершины до кратности 3,
     # Чтобы равномерно разбить их на соответствующие участки
+    POWER = n // 3
 
-    # adj_matrix = np.zeros((n, n), dtype=int)
-    # for src, target in (tuple(map(int, line.split())) for line in data):
-    #     adj_matrix[src - 1, target - 1] = 1
-    #     adj_matrix[target - 1, src - 1] = 1
-    graph_g = set()
-    for src, target in (tuple(map(int, line.split())) for line in data):
+    for src, target in (tuple(map(lambda x: int(x) - 1, line.split())) for line in data):
         graph_g.add((min(src, target), max(src, target)))
 
-    matrix_h = get_matrix_h(n, graph_g)
+    matrix_h = get_matrix_h(n)
+    pprint(matrix_h)
+    return
+
+
+if __name__ == '__main__':
+    main()
